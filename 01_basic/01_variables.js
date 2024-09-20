de75014e520b54b9569cc3de81c225935b03a61a
ashisingh123 <165305910+ashisingh123@users.noreply.github.com>
2024-09-20 09:45:01 +0000

    Variables and datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 123455;    //cannot change
+var accountemail = "ashi@gmail.com";   // prefer not to use because of issue of block scope and functionaal scope 
+let accounpassword = "ashisinh1009"
+name1 = "ashi singh" // not prefer
+let age1; //undefined
+
+console.table([accounpassword, accountId, accountemail, name1, age1])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,17 @@
+// alert(3+3) //we are using node js not browser
+
+let age = 19
+let name ="ashi"
+let islogged = true
+
+/*
+number
+string
+boolean
+undefined
+null
+object
+*/
+
+console.log(typeof null)  //null is object type
+console.log(typeof undefined)
